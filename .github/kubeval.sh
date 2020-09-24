21e9fc9ffd3e72576193f32ce6a157a5506cddee
Tosin Akinosho <takinosh@redhat.com>
2020-09-24 14:02:29 -0400

    correcting directory

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 ls -R .
 
 
-CHART_DIRS="charts/quarkuscoffeshop-charts"
+CHART_DIRS="./charts/quarkuscoffeeshop-charts"
 KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 REPO_PATH="gh-pages"
