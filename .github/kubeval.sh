2bdcc1f3ed83293132fd5c893a3bf45d26c4d041
Jeremy Davis <jeremyrdavis@mac.com>
2020-09-24 11:06:58 -0400

    updated kubeval

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CHART_DIRS="charts/quarkus-helm-chart"
+CHART_DIRS="charts/quarkuscoffeshop-helm"
 KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 REPO_PATH="gh-pages"
